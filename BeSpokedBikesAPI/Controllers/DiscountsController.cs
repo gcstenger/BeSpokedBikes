8b4733b64c5280613ce2ebec34aa12978bc23b37
gcstenger <greg@stenger.tv>
2019-10-23 14:23:08 -0400

    Initial commit of repo

---
+++ BeSpokedBikesAPI/Controllers/DiscountsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BeSpokedBikes;
+
+namespace BeSpokedBikes.Controllers
+{
+    public class DiscountsController : ApiController
+    {
+        private BeSpokedBikesEntities db = new BeSpokedBikesEntities();
+
+        // GET: api/Discounts
+        public IQueryable<Discount> GetDiscounts()
+        {
+            return db.Discounts;
+        }
+
+        // GET: api/Discounts/5
+        [ResponseType(typeof(Discount))]
+        public async Task<IHttpActionResult> GetDiscount(int id)
+        {
+            Discount discount = await db.Discounts.FindAsync(id);
+            if (discount == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(discount);
+        }
+
+        // PUT: api/Discounts/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutDiscount(int id, Discount discount)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != discount.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(discount).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DiscountExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Discounts
+        [ResponseType(typeof(Discount))]
+        public async Task<IHttpActionResult> PostDiscount(Discount discount)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Discounts.Add(discount);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = discount.Id }, discount);
+        }
+
+        // DELETE: api/Discounts/5
+        [ResponseType(typeof(Discount))]
+        public async Task<IHttpActionResult> DeleteDiscount(int id)
+        {
+            Discount discount = await db.Discounts.FindAsync(id);
+            if (discount == null)
+            {
+                return NotFound();
+            }
+
+            db.Discounts.Remove(discount);
+            await db.SaveChangesAsync();
+
+            return Ok(discount);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DiscountExists(int id)
+        {
+            return db.Discounts.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ BeSpokedBikesAPI/Controllers/SalespersonsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BeSpokedBikes;
+using System.Web.Http.Cors;
+
+namespace BeSpokedBikes.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods:"*")]
+    public class SalespersonsController : ApiController
+    {
+        private BeSpokedBikesEntities db = new BeSpokedBikesEntities();
+
+        // GET: api/Salespersons
+        public IQueryable<Salesperson> GetSalespersons()
+        {
+            return db.Salespersons;
+        }
+
+        // GET: api/Salespersons/5
+        [ResponseType(typeof(Salesperson))]
+        public async Task<IHttpActionResult> GetSalesperson(int id)
+        {
+            Salesperson salesperson = await db.Salespersons.FindAsync(id);
+            if (salesperson == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(salesperson);
+        }
+
+        // PUT: api/Salespersons/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutSalesperson(int id, Salesperson salesperson)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != salesperson.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(salesperson).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SalespersonExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Salespersons
+        [ResponseType(typeof(Salesperson))]
+        public async Task<IHttpActionResult> PostSalesperson(Salesperson salesperson)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Salespersons.Add(salesperson);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = salesperson.Id }, salesperson);
+        }
+
+        // DELETE: api/Salespersons/5
+        [ResponseType(typeof(Salesperson))]
+        public async Task<IHttpActionResult> DeleteSalesperson(int id)
+        {
+            Salesperson salesperson = await db.Salespersons.FindAsync(id);
+            if (salesperson == null)
+            {
+                return NotFound();
+            }
+
+            db.Salespersons.Remove(salesperson);
+            await db.SaveChangesAsync();
+
+            return Ok(salesperson);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SalespersonExists(int id)
+        {
+            return db.Salespersons.Count(e => e.Id == id) > 0;
+        }
+    }
+}
